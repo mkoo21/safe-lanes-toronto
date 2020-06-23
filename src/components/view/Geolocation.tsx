f817b743b95448e74d3e0447d17643d5c1a46c88
mkoo21 <koo.martin@gmail.com>
2020-06-23 00:31:11 -0400

    fucking animation wip

--- src/components/view/Geolocation.tsx
+++ src/components/view/Geolocation.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useTransition, animated } from 'react-spring'
 import { FaGlobeAmericas, FaSadTear } from 'react-icons/fa';
 
 export enum GeolocationPermissionStates {
@@ -9,8 +10,11 @@ export enum GeolocationPermissionStates {
   dismissed='dismissed',
 };
 
-const Container = styled.div`
+interface animationProps {
+  marginLeft: number;
+}
 
+const Container = animated(styled.div`
   grid-area: main;
   position: relative;
   height: 300px;
@@ -21,7 +25,9 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-`;
+  opacity: ${props => props.styles.opacity};
+  transform: ${props => props.styles.transform};
+`);
 
 const Icon = styled(FaGlobeAmericas)`
   height: 200px;
@@ -35,8 +41,9 @@ const SadIcon = styled(FaSadTear)`
   margin: 20px;
 `;
 
-export default ({setGeolocation}) => {
+export default ({ setGeolocation }) => {
   const [ permissionState, setPermissionState ] = useState(GeolocationPermissionStates.dismissed);
+
   const getGeolocation = async () => {
     // Permission check
     const status = await navigator.permissions.query({name: "geolocation"});
@@ -55,24 +62,34 @@ export default ({setGeolocation}) => {
     getGeolocation();
   }, []);
 
+  // Right-to-left animation
+  const transitions = useTransition([0, 1], null, {
+    from: { opacity: 0, transform: 'translate3d(100%, 0, 0,' },
+    enter: { opacity: 1, transform: 'translate3d(0%, 0, 0,' },
+    leave: { opacity: 0, transform: 'translate3d(-50%, 0, 0,' },
+  });
+  debugger;
 
   if(permissionState === GeolocationPermissionStates.dismissed || permissionState === GeolocationPermissionStates.granted) return null;
   if(permissionState === GeolocationPermissionStates.denied) {
-    return <GeolocationDenied />
+    return <GeolocationDenied style={transitions[1].props} />
   }
   return ( 
-    <Container>
-      <Icon />
-      <div>
-        <h3>This app works best with location enabled.</h3>
-        <button>OK</button>
-      </div>
-    </Container>
+    <GeolocationPrompt style={transitions[0].props} />
   )
 }
 
+export const GeolocationPrompt = () => <Container>
+  <Icon />
+  <div>
+    <h3>This app works best with location enabled.</h3>
+    <button>OK</button>
+  </div>
+</Container>
+
+
 export const GeolocationDenied = () => <Container>
   <SadIcon />
-  <div>Looks like you denied us location access. If you change your mind you can change site permissions in your browser settings under "Privacy and Security."</div>
+  <h3>Looks like you denied us location access. If you change your mind you can change site permissions in your browser settings under "Privacy and Security."</h3>
   <button>OK</button>
 </Container>
