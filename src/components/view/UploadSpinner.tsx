6afb3ab9da74d0f0f2f5647fa82ddabaa037e9f2
mkoo21 <koo.martin@gmail.com>
2020-07-10 14:07:54 -0400

    material ui spinner

--- src/components/view/UploadSpinner.tsx
+++ src/components/view/UploadSpinner.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useTransition, animated } from 'react-spring';
 import { Keyframes } from 'react-spring/renderprops'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const SIZE = 200;
 const Container = styled.div`
   grid-area: main;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
 `;
 
 const Border = styled(animated.div)`
@@ -20,17 +21,18 @@ const Border = styled(animated.div)`
   width: ${SIZE}px;
 `;
 
-const H4 = styled.h4`martinTop: 30px;`
+const H3 = styled.h3`margin-top: 20px;`
 
 export default () => {
-  const Spin = Keyframes.Spring(async (next: any) => {
-    while(true) {
-      await next({transform: "rotate(0deg)", from: {transform: "rotate(360deg)"}, reset: true })
-    }
-  });
+  // const Spin = Keyframes.Spring(async (next: any) => {
+  //   while(true) {
+  //     await next({transform: "rotate(0deg)", from: {transform: "rotate(360deg)"}, reset: true })
+  //   }
+  // });
 
   return <Container>
-    <Spin config={{tension:120}}>{ (styles: any) => <Border style={styles} /> }</Spin>
-    <H4>Uploading...</H4>
+    {/* <Spin config={{tension:120}}>{ (styles: any) => <Border style={styles} /> }</Spin> */}
+    <CircularProgress size={80} />
+    <H3>Uploading...</H3>
   </Container>;
 };
